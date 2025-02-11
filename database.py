22deeae33aa40df9ac356b60ceff2582d739a9e7
tzwf0819 <825662922@qq.com>
2025-02-11 21:10:44 +0800

    临时服务器连接成功

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlmodel import create_engine, SQLModel, Session, select
 from list_models import ListItem
 from models import ProductCategory
 # MSSQL数据库连接字符串
-mssql_url = "mssql+pyodbc://sa:shaoyansa@localhost:1433/yida?driver=ODBC+Driver+17+for+SQL+Server"
+#mssql_url = "mssql+pyodbc://sa:shaoyansa@localhost:1433/yida?driver=ODBC+Driver+17+for+SQL+Server"
+mssql_url = "mssql+pyodbc://sa:ABCabc123@150.158.136.4:1433/yida?driver=ODBC+Driver+17+for+SQL+Server&Encrypt=no&TrustServerCertificate=no"
 engine = create_engine(mssql_url, echo=True)
 
 def get_session():
